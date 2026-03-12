eeb8cc0257b802e0304636086eda14311d6c0d1a
Nikhil kumar <nikhilkumar.cout@gmail.com>
2026-03-12 15:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (10.99%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -7,13 +7,11 @@ public:
     }
     
     void push(int x) {
-       q2.push(x);
-       while(!q1.empty()){
-        q2.push(q1.front());
+        q1.push(x);
+      for(int i=0;i<q1.size()-1;i++){
+        q1.push(q1.front());
         q1.pop();
-       }
-       swap(q1,q2);
-        
+      }
     }
     
     int pop() {
